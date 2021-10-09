d23c88185c907d0f22a0b895837454409d58f755
antmyers <antmyers@umich.edu>
2021-10-09 09:37:37 -0400

    updated locations

--- src/AccordionAfternoon.js
+++ src/AccordionAfternoon.js
@@ -14,11 +14,11 @@ function AccordionAfternoon(props) {
       <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
         <div class="accordion-body">
           <ul class="list-group">
-            <ScheduleElement title="Lunch!!" location="Newman Hall" leaders="Wade Gerberding" time="12:40pm" />
+            <ScheduleElement title="Lunch!!" location="SMSP Courtyard" leaders="Wade Gerberding" time="12:40pm" />
             <ScheduleElement title="Freestyle Prayer Stations" location="Various" leaders="Various" time="1:30pm" />
-            <ScheduleElement title="Student Testimony 2" location="SMSP Courtyard" leaders="Mary Wendelowski" time="2:40pm" />
-            <ScheduleElement title="Ignatian Examen" location="SMSP Courtyard" leaders="Bailey" time="2:50pm" />
-            <ScheduleElement title="Reflection" location="SMSP Courtyard" leaders="Yourself!" time="3:05pm" />
+            <ScheduleElement title="Student Testimony 2" location="Donnely Hall" leaders="Mary Wendelowski" time="2:40pm" />
+            <ScheduleElement title="Ignatian Examen" location="Donnely Hall" leaders="Bailey" time="2:50pm" />
+            <ScheduleElement title="Reflection" location="Donnely Hall" leaders="Yourself!" time="3:05pm" />
             <ScheduleElement title="Small Group Discussion" location="See Small Groups Tab" leaders="Small Group Leaders" time="3:15pm" />
             <ScheduleElement title="Free Time" location="Various" leaders="Yourself!" time="3:45pm" />
           </ul>
--- src/AccordionMorning.js
+++ src/AccordionMorning.js
@@ -14,12 +14,12 @@ function AccordionMorning(props) {
       <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
         <div class="accordion-body">
           <ul class="list-group">
-            <ScheduleElement title="Check-In" location="Newman Lounge" leaders="The Retreats Team" time="10:00am" />
+            <ScheduleElement title="Check-In" location="SMSP Courtyard" leaders="The Retreats Team" time="10:00am" />
             <ScheduleElement title="Introductions" location="SMSP Courtyard" leaders="The Retreats Team" time="10:30am" />
             <ScheduleElement title="Icebreakers" location="SMSP Courtyard" leaders="Bekah Riley" time="10:45am" />
-            <ScheduleElement title="Student Testimony" location="SMSP Courtyard" leaders="Joey Imperial" time="11:25am" />
+            <ScheduleElement title="Breathing and Yoga" location="Donnely Hall" leaders="Sam Weians" time="11:15am" />
+            <ScheduleElement title="Student Testimony" location="Donnely Hall" leaders="Joey Imperial" time="11:25am" />
             <ScheduleElement title="Small Group Discussion" location="See Small Groups Tab" leaders="Small Group Leaders" time="11:45am" />
-            <ScheduleElement title="Activity" location="SMSP Courtyard" leaders="???" time="12:15pm" />
           </ul>
         </div>
       </div>
